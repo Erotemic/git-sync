755844702ee1d4d87a74c3efd5aa40bb4a65b6f9
joncrall <erotemic@gmail.com>
2023-01-19 18:44:01 -0500

    wip

--- run_tests.py
+++ run_tests.py
@@ -3,7 +3,7 @@ if __name__ == '__main__':
     import pytest
     import sys
     package_name = 'git_sync'
-    mod_dpath = package_name
+    mod_dpath = 'git_sync'
     test_dpath = 'tests'
     pytest_args = [
         '--cov-config', 'pyproject.toml',
